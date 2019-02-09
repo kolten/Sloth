021d3703e992fa7d83b46c969e100c322efe0b7a
Alex Pavlak <alexander.pavlak@mavs.uta.edu>
2019-02-09 17:52:18 -0600

    Base class for keyboard recycler view

---
+++ app/src/main/java/me/koltensturgill/sloth/ui/editor/KeyboardButton.java
@@ -0,0 +1,15 @@
+package me.koltensturgill.sloth.ui.editor;
+
+public class KeyboardButton {
+
+    private String mText;
+
+    public KeyboardButton(String text){
+
+        mText = text;
+    }
+
+    public String getText(){
+        return mText;
+    }
+}
