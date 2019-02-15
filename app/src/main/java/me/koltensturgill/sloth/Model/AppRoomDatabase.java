c74354904c054435bbce525bd934bceef26fe1b1
Kolten Sturgill <koltensturgill92@gmail.com>
2019-02-15 13:27:07 -0600

    renamed NoteRoomDatabase -> AppRoomDatabase, set up NoteRepo, starting note view model

--- app/src/main/java/me/koltensturgill/sloth/Model/AppRoomDatabase.java
+++ app/src/main/java/me/koltensturgill/sloth/Model/AppRoomDatabase.java
@@ -5,19 +5,20 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
+// Add more entities here
 @Database(entities = {Note.class}, version = 1, exportSchema = false)
-public abstract class NoteRoomDatabase extends RoomDatabase {
+public abstract class AppRoomDatabase extends RoomDatabase {
     public abstract NoteDao noteDao();
 
-    private static volatile NoteRoomDatabase INSTANCE;
+    private static volatile AppRoomDatabase INSTANCE;
 
     // Create a singleton instance of the Room database
-    static NoteRoomDatabase getInstance(final Context context){
+    static AppRoomDatabase getInstance(final Context context){
         if (INSTANCE == null){
-            synchronized (NoteRoomDatabase.class){
+            synchronized (AppRoomDatabase.class){
                 if (INSTANCE == null){
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    NoteRoomDatabase.class, "sloth_database").build();
+                    AppRoomDatabase.class, "sloth_database").build();
                 }
             }
         }
---
+++ app/src/main/java/me/koltensturgill/sloth/Model/NoteViewModel.java
@@ -0,0 +1,11 @@
+package me.koltensturgill.sloth.Model;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class NoteViewModel extends AndroidViewModel {
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
