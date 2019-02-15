693b939a0db85673e87677eabf3ed20b5c15ac57
Kolten Sturgill <koltensturgill92@gmail.com>
2019-02-15 13:30:48 -0600

    NoteViewModel set up

--- app/src/main/java/me/koltensturgill/sloth/Model/NoteViewModel.java
+++ app/src/main/java/me/koltensturgill/sloth/Model/NoteViewModel.java
@@ -2,10 +2,26 @@ package me.koltensturgill.sloth.Model;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
 public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository mRepository;
+    private LiveData<List<Note>> mAllNotes;
+
     public NoteViewModel(@NonNull Application application) {
         super(application);
+        mRepository = new NoteRepository(application);
+        mAllNotes = mRepository.getAllNotes();
     }
+
+    // Getters to abstract request from UI
+    public LiveData<List<Note>> getAllNotes() {
+        return mAllNotes;
+    }
+
+    // Wrapper for insert method in repo
+    public void insert(Note note) { mRepository.insert(note); }
 }
