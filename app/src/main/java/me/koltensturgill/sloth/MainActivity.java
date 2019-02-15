b5e3544491928a094f67ef1bdcb2b57d28c2de9d
hjpatel16 <hjpatel16@myseneca.ca>
2019-02-15 02:28:02 -0500

    Fixed-Issue-17 where user will now be redirected to settings page. Added Settings.java and activity_settings.xml and modified MainActivity.java

--- app/src/main/java/me/koltensturgill/sloth/MainActivity.java
+++ app/src/main/java/me/koltensturgill/sloth/MainActivity.java
@@ -81,7 +81,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            final Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
