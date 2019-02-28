9328eb9887c3c80197333eb18bd0f0ac95138e8f
Alex Pavlak <alexander.pavlak@mavs.uta.edu>
2019-02-28 13:22:15 -0600

    Moved contents of editor fragment to editor activity

--- app/src/main/java/me/koltensturgill/sloth/Editor.java
+++ app/src/main/java/me/koltensturgill/sloth/Editor.java
@@ -2,19 +2,36 @@ package me.koltensturgill.sloth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 import me.koltensturgill.sloth.ui.editor.EditorFragment;
+import me.koltensturgill.sloth.ui.editor.EditorViewModel;
+import me.koltensturgill.sloth.ui.editor.KeyboardButton;
 
 public class Editor extends AppCompatActivity {
+
+    private EditorViewModel mViewModel;
+    ArrayList<KeyboardButton> buttons;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editor_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, EditorFragment.newInstance())
-                    .commitNow();
+
+        //Setup for the keyboard button bar recycler view
+        editText = (EditText) findViewById(R.id.et_editor);
+        RecyclerView buttonBar = (RecyclerView) findViewById(R.id.rvKeyboardBar);
+        buttons = KeyboardButton.createButtonList();
+        KeyboardBarAdapter rvAdapter = new KeyboardBarAdapter(buttons, editText);
+        buttonBar.setAdapter(rvAdapter);
+        buttonBar.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL,false));
+
         }
-    }
+
 }
